708b200a358a50731774477b574a65cc06624581
leen18-meet <kassisleen@gmail.com>
2017-11-02 18:41:46 +0200

    1st thing

---
+++ 2Java.js
@@ -0,0 +1,13 @@
+alert('i dont have a favorate movie');
+
+var n1 = 4;
+var n2 = 16;
+var s1 = ‘Ninja’;
+var s2 = ‘Turtle’;
+
+alert(s1 + s2);
+alert(n1 + n2);
+alert(n1 + s1);
+alert(s1 + n1);
+alert(n1 + s1 + n2);
+alert(n1 + n2 + s1);
